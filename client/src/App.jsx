b84a4d4f345511eb1071efa057d4f21058a50262
NicolasPham <phd.ngoc163@gmail.com>
2023-02-03 18:04:51 -0500

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React from "react";
 
 //pages
-import CreatePost from './pages/CreatePost';
-import Home from './pages/Home';
+import CreatePost from "./pages/CreatePost";
+import Home from "./pages/Home";
 
-import {logo} from './assets'
+import { logo } from "./assets";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center sm:px-8 px-4 py-4 bg-white border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img className="w-28 object-contain" src={logo} alt="logo" />
+        </Link>
 
-export default App
+        <Link
+          to="/create-post"
+          className="font-inter font-medium text-white bg-[#6469ff] px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
+      </header>
+
+      <main className="w-full bg-[#f9fafe] sm:p-8 px-4 py-8 min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
+
+export default App;
